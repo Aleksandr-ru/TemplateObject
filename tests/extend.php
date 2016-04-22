4660af2c8be41987087d60652041c5d9506d9c0e
Aleksandr-ru <rebel@pisem.net>
2016-04-22 15:34:04 +0300

    Extend tests

---
+++ tests/extend.php
@@ -0,0 +1,17 @@
+<?php
+//error_reporting(E_ALL);
+//ini_set('display_errors', 'on');
+//header("content-type: text/plain");
+require('../TemplateObject.php');
+
+$to = TemplateObject::loadTemplate('extend.html');
+
+$to->setVariable('TITLE', "this is a 'title'");
+
+for($i=1; $i<=3; $i++) {
+    $row = $to->setBlock('row');    
+    $row->setVariable('COL', "test \"$i\"");
+}
+
+$to->showOutput();
+?>
