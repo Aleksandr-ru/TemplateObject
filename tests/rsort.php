04594038491aad45c8064d470b6a5e581c75cc09
Aleksandr-ru <rebel@pisem.net>
2016-07-14 14:31:45 +0300

    block options test

---
+++ tests/rsort.php
@@ -0,0 +1,14 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+//header("content-type: text/plain");
+require('../TemplateObject.php');
+
+$to = TemplateObject::loadTemplate('rsort.html');
+
+for($i=1; $i<=3; $i++) {
+	$b = $to->setBlock('normal')->setVariable('I', $i);
+	$b = $to->setBlock('reversed')->setVariable('I', $i);
+}
+$to->showOutput();
+?>
