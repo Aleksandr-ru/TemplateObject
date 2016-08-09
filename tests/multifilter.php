5e369062ca65ca7d0d818193072dbd4c9ee96ecd
Aleksandr-ru <rebel@pisem.net>
2016-08-09 13:19:19 +0300

    tests

--- tests/multifilter.php
+++ tests/multifilter.php
@@ -1,8 +1,9 @@
 <?php
 //header("content-type: text/plain");
 require('../TemplateObject.php');
-$to = new TemplateObject();
-$to->loadTemplate('multifilter.html');
+//$to = new TemplateObject();
+//$to->loadTemplate('multifilter.html');
+$to = TemplateObject::loadTemplate('multifilter.html');
 $to->setVariable('TITLE', "Multiple filter test");
 
 $string = "This a string with \"quotes\" and several lines\nsecond line\nthitd line";
@@ -10,8 +11,23 @@ $to->setVariable('VAR', $string);
 
 $to->addFilter('wrongfilter', function($a){ return 'wrong! '.$a; });
 $to->removeFilter('raw');
-$to->addFilter('html', 'htmlentities', TRUE);
+$to->addFilter('html', 'html_cb', TRUE);
+$to->addFilter('html2', 'html2_cb');
+
+for($i=1; $i<=3; $i++) {
+	$b = $to->setBlock('block1');
+	$b->setVariable('VAR', $string);
+}
 
 $to->showOutput();
 
+function html_cb($a)
+{
+	return '<font color=red>' . htmlentities($a) . '</font>';
+}
+
+function html2_cb($a)
+{
+	return '<font color=green>' . htmlentities($a) . '</font>';
+}
 ?>
